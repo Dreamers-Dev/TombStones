9d6d5eb9b5db85f039219eaaae4731798d54fd47
Rafi <rafiei.default@gmail.com>
2025-01-10 21:29:42 +0330

    - Initial commit

---
+++ src/main/kotlin/dev/dreamers/tombStones/TombStones.kt
@@ -0,0 +1,14 @@
+package dev.dreamers.tombStones
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TombStones : JavaPlugin() {
+
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+}
